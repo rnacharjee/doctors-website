749754808b53c4b938fc73a550e28fc76cee9915
rnacharjee <rajib.ipe@gmail.com>
2022-10-29 10:11:30 +0600

    added environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,11 @@
+console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyAWtwvJfqrIIytsHHRuGUHpVYQhwAn8Urw",
-  authDomain: "dr-priyanka-website.firebaseapp.com",
-  projectId: "dr-priyanka-website",
-  storageBucket: "dr-priyanka-website.appspot.com",
-  messagingSenderId: "773832782798",
-  appId: "1:773832782798:web:5d644748fc81d2d639319f",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
